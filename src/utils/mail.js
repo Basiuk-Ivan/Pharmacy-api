d8662d3b77799379ad1f8cb81553c5c8b8bdd2d7
IvanBasss <122455382+Basiuk-Ivan@users.noreply.github.com>
2023-07-02 17:21:37 +0300

    new

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -2,12 +2,12 @@ import nodemailer from 'nodemailer';
 import HTML_TEMPLATE from './mailTemplate.js';
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
+  host: 'smtp.ukr.net',
+  port: 2525,
   secure: true,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    user: 'pharmacy_shop@ukr.net',
+    pass: '73tOm3Qhbe71AhIn',
   },
   // tls: { rejectUnauthorized: false },
 });
